73007d9111a42f793c04c26f529e7cc930615084
Jesse White <jesse@opennms.org>
2021-10-15 10:35:58 -0400

    initial commit from OIA archetype
    mvn archetype:generate -B -DarchetypeGroupId=org.opennms.integration.api -DarchetypeArtifactId=example-kar-plugin -DarchetypeVersion=0.6.0 -DgroupId=org.opennms.plugins.zabbix -DartifactId=zabbix-plugin -Dversion=1.0.0-SNAPSHOT -Dpackage=org.opennms.plugins.zabbix -DpluginId=zabbix -DpluginName="Zabbix"

---
+++ plugin/src/main/java/org/opennms/plugins/zabbix/shell/StatsCommand.java
@@ -0,0 +1,31 @@
+package org.opennms.plugins.zabbix.shell;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.opennms.plugins.zabbix.AlarmForwarder;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+
+@Command(scope = "opennms-zabbix", name = "stats", description = "Show statistics.")
+@Service
+public class StatsCommand implements Action {
+
+    @Reference
+    private AlarmForwarder forwarder;
+
+    @Override
+    public Object execute() {
+        final MetricRegistry metrics = forwarder.getMetrics();
+        final ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+        reporter.report();
+        return null;
+    }
+}
---
+++ plugin/src/main/java/org/opennms/plugins/zabbix/shell/TopologyCommand.java
@@ -0,0 +1,39 @@
+package org.opennms.plugins.zabbix.shell;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.opennms.plugins.zabbix.TopologyForwarder;
+import org.opennms.plugins.zabbix.model.Topology;
+
+@Command(scope = "opennms-zabbix", name = "push-topology", description = "Push the topology")
+@Service
+public class TopologyCommand implements Action {
+
+    @Reference
+    private TopologyForwarder forwarder;
+
+    @Override
+    public Object execute() {
+        System.out.println("Forwarding topologies...");
+        final CompletableFuture<List<Topology>> future = forwarder.forwardTopologies();
+        future.whenComplete((topologies,ex) -> {
+            if (ex == null) {
+                System.out.printf("Successfully forwarded %d topologies.", topologies.size());
+            } else {
+                System.out.println("Error occurred forwarding topologies: " + ex);
+                ex.printStackTrace();
+            }
+        });
+        try {
+            future.get();
+        } catch (Exception e) {
+            // pass
+        }
+        return null;
+    }
+}
