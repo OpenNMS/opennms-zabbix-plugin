444e5bbdf42cbb6380190a4fefb0491c91db8839
Jesse White <jesse@opennms.org>
2021-10-21 20:37:15 -0400

    better, simpler

--- plugin/src/test/java/org/opennms/plugins/zabbix/expressions/ZabbixExpressionParserTest.java
+++ plugin/src/test/java/org/opennms/plugins/zabbix/expressions/ZabbixExpressionParserTest.java
@@ -11,17 +11,18 @@ public class ZabbixExpressionParserTest {
 
     @Test
     public void canParseExpression() throws org.opennms.plugins.zabbix.expressions.ParseException {
-        Expression expression = parser.parse("max(123) + 5");
+        Expression expression = parser.parse("max(123,1) + 5");
         assertThat(expression.getName(), equalTo("max"));
-        assertThat(expression.getParameters(), contains(new Parameter("123")));
+        assertThat(expression.getParameters(), contains(new Parameter("123"), new Parameter("1")));
         assertThat(expression.getOperator(), equalTo("+"));
         assertThat(expression.getConstant(), equalTo("5"));
 
         expression = parser.parse("min(/Linux block devices by Zabbix agent/vfs.dev.read.await[{#DEVNAME}],15m) > {$VFS.DEV.READ.AWAIT.WARN:\"{#DEVNAME}\"}");
         assertThat(expression.getName(), equalTo("min"));
-//        assertThat(expression.getParameters(), contains(
-//                new Parameter("/Linux block devices by Zabbix agent/vfs.dev.read.await[{#DEVNAME}]"),
-//                new Parameter("15m")));
+        System.out.println(expression.getParameters());
+        assertThat(expression.getParameters(), contains(
+                new Parameter("/Linux block devices by Zabbix agent/vfs.dev.read.await[{#DEVNAME}]"),
+                new Parameter("15m")));
         assertThat(expression.getOperator(), equalTo(">"));
         assertThat(expression.getConstant(), equalTo("{$VFS.DEV.READ.AWAIT.WARN:\"{#DEVNAME}\"}"));
     }
