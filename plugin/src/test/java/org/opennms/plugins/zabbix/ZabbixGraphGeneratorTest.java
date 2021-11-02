07f69f959b80eb4997fc8e6489d55f950dffa80b
Jesse White <jesse@opennms.org>
2021-11-01 21:51:56 -0400

    fix tests

--- plugin/src/test/java/org/opennms/plugins/zabbix/ZabbixGraphGeneratorTest.java
+++ plugin/src/test/java/org/opennms/plugins/zabbix/ZabbixGraphGeneratorTest.java
@@ -24,18 +24,18 @@ public class ZabbixGraphGeneratorTest {
         ZabbixGraphGenerator zabbixGraphGenerator = new ZabbixGraphGenerator();
         Graph graph = netIfDiscovery.getGraphPrototypes().get(0);
         PrefabGraph prefabGraph = zabbixGraphGenerator.toPrefabGraph(graph, netIfDiscovery);
-        assertThat(prefabGraph.getCommand(), equalTo("--title=\"Interface {#IFNAME}: Network traffic\" \\\n" +
-                "DEF:m0={rrd1}:net_if_in:AVERAGE \\\n" +
-                "DEF:m1={rrd1}:net_if_in_dropped:AVERAGE \\\n" +
-                "DEF:m2={rrd1}:net_if_in_errors:AVERAGE \\\n" +
-                "DEF:m3={rrd1}:net_if_out:AVERAGE \\\n" +
-                "DEF:m4={rrd1}:net_if_out_dropped:AVERAGE \\\n" +
-                "DEF:m5={rrd1}:net_if_out_errors:AVERAGE \\\n" +
-                "LINE1:m0#4e9a06:\"net_if_in\" \\ \n" +
-                "LINE1:m1#4e9a06:\"net_if_in_dropped\" \\ \n" +
-                "LINE1:m2#4e9a06:\"net_if_in_errors\" \\ \n" +
-                "LINE1:m3#4e9a06:\"net_if_out\" \\ \n" +
-                "LINE1:m4#4e9a06:\"net_if_out_dropped\" \\ \n" +
-                "LINE1:m5#4e9a06:\"net_if_out_errors\" \\ \n"));
+        assertThat(prefabGraph.getCommand(), equalTo("--title=\"Interface {#IFNAME}: Network traffic\"\n" +
+                "DEF:m0={rrd1}:net_if_in:AVERAGE\n" +
+                "DEF:m1={rrd1}:net_if_in_dropped:AVERAGE\n" +
+                "DEF:m2={rrd1}:net_if_in_errors:AVERAGE\n" +
+                "DEF:m3={rrd1}:net_if_out:AVERAGE\n" +
+                "DEF:m4={rrd1}:net_if_out_dropped:AVERAGE\n" +
+                "DEF:m5={rrd1}:net_if_out_errors:AVERAGE\n" +
+                "LINE1:m0#4e9a06:\"net_if_in\"\n" +
+                "LINE1:m1#4e9a06:\"net_if_in_dropped\"\n" +
+                "LINE1:m2#4e9a06:\"net_if_in_errors\"\n" +
+                "LINE1:m3#4e9a06:\"net_if_out\"\n" +
+                "LINE1:m4#4e9a06:\"net_if_out_dropped\"\n" +
+                "LINE1:m5#4e9a06:\"net_if_out_errors\"\n"));
     }
 }
